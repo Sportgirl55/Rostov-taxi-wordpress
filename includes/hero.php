429d5ca8b36d8dc772193eee7beca788b2467cd4
Elena Slyusareva <sportgirl55@bk.ru>
2021-02-16 23:09:06 +0500

    Small changes

--- includes/hero.php
+++ includes/hero.php
@@ -1,5 +1,5 @@
 <section class="hero">
-  <h1 class="main-title">Заказ трансфера<span> в Ростове-на-Дону</span></h1>
+  <h1 class="main-title"><?php the_field('glavnyj_zagolovok'); ?></h1>
   <div class="messengers messengers__hero">
     <?php include 'messengers.php'?>
   </div>
